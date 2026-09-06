c4e62786fa57067d734fcabe2c8b3d0a883a0f1d
ravikanthyg <ravikanth5798@gmail.com>
2026-09-06 16:16:40 +0530

    git commit -a

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,10 @@
 import Welcome from "./components/welcome"
+import Greet from "./components/Greet"
 
 const App = () => {
   return <div>
     <Welcome />
+    <Greet />
   </div>
 }
 export default App
---
+++ my-react-app/src/components/Greet.jsx
@@ -0,0 +1,4 @@
+const Greet = () => {
+    return <h2>Good Morning 🌅</h2>
+}
+export default Greet
